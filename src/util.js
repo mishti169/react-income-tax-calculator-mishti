55efa14cffdac851722cca923a6129f49709215e
MANISHA <manishapanchal169@gmail.com>
2022-06-08 18:13:37 +0530

    add getCalculatedNewTax function for as per new regime tax

--- src/util.js
+++ src/util.js
@@ -17,3 +17,28 @@ export const getCalculatedTax = (income = 0) => {
 	}
 	return calculatedTax;
 };
+
+export const getCalculatedNewTax = (income = 0) => {
+	const incomeAfterDeduction = income - 50000;
+	if (incomeAfterDeduction <= 250000) {
+		return 0;
+	} else if (incomeAfterDeduction > 250000 && incomeAfterDeduction <= 500000) {
+		return (incomeAfterDeduction - 250000) * 0.05;
+	} else if (incomeAfterDeduction > 500000 && incomeAfterDeduction <= 750000) {
+		return (incomeAfterDeduction - 500000) * 0.1 + 12500;
+	} else if (incomeAfterDeduction > 750000 && incomeAfterDeduction <= 1000000) {
+		return (incomeAfterDeduction - 750000) * 0.15 + 37500;
+	} else if (
+		incomeAfterDeduction > 1000000 &&
+		incomeAfterDeduction <= 1250000
+	) {
+		return (incomeAfterDeduction - 1000000) * 0.2 + 75000;
+	} else if (
+		incomeAfterDeduction > 1250000 &&
+		incomeAfterDeduction <= 1500000
+	) {
+		return (incomeAfterDeduction - 1250000) * 0.25 + 125000;
+	} else if (incomeAfterDeduction > 1500000) {
+		return (incomeAfterDeduction - 1500000) * 0.3 + 187500;
+	}
+};
