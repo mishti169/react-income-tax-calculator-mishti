b3f0439c384fbb6d74a3812b5beb474e3a6642b7
MANISHA <manishapanchal169@gmail.com>
2022-06-09 13:36:23 +0530

    add deduction and exemption

--- src/App.js
+++ src/App.js
@@ -27,16 +27,34 @@ function App() {
 			<form>
 				<label>Total Income</label>
 				<input value={inputVal} onChange={onChangeInputVal} />
+				<label>Standard Deduction</label>
+				<input value={inputVal} onChange={onChangeInputVal} />
+				<label>HRA</label>
+				<input value={inputVal} onChange={onChangeInputVal} />
+				<label>PT and Entertainment Allowance</label>
+				<input value={inputVal} onChange={onChangeInputVal} />
+				<label>NPS</label>
+				<input value={inputVal} onChange={onChangeInputVal} />
+				<label>leave Travel Allowance</label>
+				<input value={inputVal} onChange={onChangeInputVal} />
+				<label>Home Loan Interest Payment</label>
+				<input value={inputVal} onChange={onChangeInputVal} />
+				<label>Deduction u/s 80C</label>
+				<input value={inputVal} onChange={onChangeInputVal} />
+				<label>Health Insurance u/s 80TTA</label>
+				<input value={inputVal} onChange={onChangeInputVal} />
 				<button type='submit' className='' onClick={onBtnClick}>
 					Calculate
 				</button>
 			</form>
-			<p>
-				your income tax as per old regime is Rs. <span>{oldTaxAmount}</span>
-			</p>
-			<p>
-				your income tax as per new regime is Rs. <span>{newTaxAmount}</span>
-			</p>
+			<div className='tax-ans-wrapper'>
+				<p>
+					your income tax as per old regime is Rs. <span>{oldTaxAmount}</span>
+				</p>
+				<p>
+					your income tax as per new regime is Rs. <span>{newTaxAmount}</span>
+				</p>
+			</div>
 		</div>
 	);
 }
